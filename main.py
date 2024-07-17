f415b9d1d809adcb752ccbe5c1ca5b528d752f1d
YounesMDev <younes.elmehdi@outlook.com>
2024-07-17 10:06:01 +0200

    check git pull

---
+++ main.py
@@ -0,0 +1,22 @@
+def welcome():
+    print("*"*80,"\n")
+    print(" "*22, "Welcome to Serial Port Checker app\n")
+    print("*" * 80, "\n")
+
+
+def load_config():
+    pass
+
+
+def check_port():
+    pass
+
+
+def main():
+    welcome()
+    load_config()
+    check_port()
+
+
+if __name__ == "__main__":
+    main()
